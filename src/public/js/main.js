549df9fb002410e76b0f09fe7491a7a936c0f1a3
Edith707 <edith.lopezhuerta@gmail.com>
2018-10-14 21:37:37 -0500

    agregando validacion de login y funciones al formulario logIn

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,2 +1,49 @@
 //ejecutando socket
-io();
+
+$(function () {
+    
+    const socket = io();
+    //obteniendo los elementos del DOM desde la interface
+
+     const $messageForm = $("#message-form");
+     const $messageBox= $("#message");
+     const $chat = $("#chat");
+
+     /*obteniendo DOM de los elementos para el nickNameForm*/
+     const $nickForm = $("#nickForm");
+     const $nickError = $("#nickError");
+     const $nickName = $("#nickName");
+     
+     const $users = $("#usersNames");
+     
+     $nickForm.submit(e => {
+         e.preventDefault();
+         socket.emit("nuevo usuario", $nickName.val(), data => {
+              if(data){
+                $("#logIn").hide();
+                $("#contentWrap").show();
+              }else {
+                $nickError.html(
+                `<div class="alert alert-danger">
+                  El nombre de usuario ya Existe.
+                </div>
+              `);   
+              }
+              $nickName.val("");
+         });
+     });
+
+     //Capturando eventos y capturando los datos del form
+     $messageForm.submit( e => {
+       e.preventDefault();
+       socket.emit("send message", $messageBox.val());
+       $messageBox.val(" ");
+       
+     }); 
+
+     //recibiendo el new message
+     socket.on("new message", function(data){
+       $chat.append(data + "<br/>clear");
+     });
+
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,7 +1,26 @@
 //exporto soquets
 module.exports = function (io) {
+    let nickName = [];
     //conectando a socket en tiempo real
     io.on("connection", socket => {
         console.log("Un nuevo usuario se ha conectado");
+        
+        socket.on("nuevo usuario", (data, cb) =>{
+            
+            if(nickName.indexOf(data)!= -1){
+             cb(false);
+            }else {
+                cb(true);
+                socket.nickName = data;
+                nickName.push(socket.nickName);
+            }
+        });
+
+        //recibiendo el mensaje de send message RECIBIENDO DATOS DEL SERVIDOR
+        socket.on("send message", function(data){
+           //Retransmitiendo el mensaje recibido a mis demas usuarios
+            io.sockets.emit("new message", data);
+        });
+        
     });
 }
